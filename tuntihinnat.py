2cf8d209d784222ee190b04c96b694ce48c73748
Salla Huuhilo <104681906+sallariina@users.noreply.github.com>
2024-10-05 23:52:21 +0300

    vuorokauden tuntihinnat

--- tuntihinnat.py
+++ tuntihinnat.py
@@ -1,7 +1,7 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
-def get_tuntihinta():
+def get_tuntihintanyt():
     url = 'https://api.porssisahko.net/v1/price.json?'
     pvm = datetime.now()
     parametrit = 'date='+ str(pvm.strftime("%Y-%m-%d")) + "&hour=" + str(pvm.strftime("%H"))
@@ -11,12 +11,39 @@ def get_tuntihinta():
         response = requests.get(url)
         if response.status_code == 200:
             tuntihinta = response.json()
-            print(f'Tuntihinta nyt {tuntihinta["price"]} snt/kWh')
+            tuntinyt = pvm.strftime("%H.%M")
+            print(f'Tuntihinta nyt ({tuntinyt}) on {tuntihinta["price"]} snt/kWh')
         else:
             print('Error:', response.status_code)
             return None
     except:
         return None
+    
+def get_tuntihinta(pvm: str, tunti: str):
+    url = 'https://api.porssisahko.net/v1/price.json?'
+    parametrit = 'date='+ str(pvm) + "&hour=" + str(tunti)
+    url += parametrit
+
+    tuntihinta = ''
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            tuntihinta = response.json()
+            return print(f'{tuntihinta["price"]} snt/kWh')
+        else:
+            return None
+    except:
+        return None
+
+def get_paivanhinnat():
+    tunti = 0
+    pvm = datetime.now().strftime("%Y-%m-%d")
+    while tunti < 24:
+        get_tuntihinta(pvm,tunti)
+        tunti += 1
+
+    return None
 
 if __name__ == "__main__":
-    get_tuntihinta()
+    get_tuntihintanyt()
+    get_paivanhinnat()
